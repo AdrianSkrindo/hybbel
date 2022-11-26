f4331c25953a98b570b8d3b172e3fa75cdf6bfd5
AdrianSkrindo <adriansli@uia.no>
2022-11-26 16:23:05 +0100

    Små fix

--- www/pages/hjem.php
+++ www/pages/hjem.php
@@ -77,7 +77,7 @@ include "../assets/inc/standar.include.php";
             echo "<h2>" . $hybel->navn . "</h2>";
             echo "<p>" . $hybel->adresse . "</p>";
             echo "<p>" . $hybel->pris . ",-</p>";
-            echo "<p>" . $hybel->hybel_id . "</p>";
+            //echo "<p>" . $hybel->hybel_id . "</p>";
             echo "</div>";
             echo "</card>";
             echo "</a>";
