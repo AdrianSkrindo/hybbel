d5410a8ce1770c0ad4191d024169fc64a926e97b
AdrianSkrindo <adriansli@uia.no>
2022-11-28 16:57:34 +0100

    Fikset login, til pdo

--- www/index.php
+++ www/index.php
@@ -1,6 +1,55 @@
 <?php
+
 session_start();
-$conn = mysqli_connect('localhost', 'root', '', 'hybelprosjekt') or die("Kunne ikke koble til database.");
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'hybelprosjekt');
+$dsn = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST; // Driver is set here
+
+try {
+  $pdo = new PDO($dsn, DB_USER, DB_PASS);
+} catch (PDOException $e) {
+  echo 'Error connecting to database: ' . $e->getMessage(); // Never do this in production
+}
+
+
+if (isset($_POST['login'])) {
+
+  $brukernavn = $_POST['brukernavn'];
+  $sql = "SELECT * FROM brukere WHERE brukernavn = '$brukernavn'";
+  $q = $pdo->prepare($sql);
+
+
+  try {
+    $q->execute();
+  } catch (PDOException $e) {
+    echo $e->getMessage() . "<br>";
+  }
+
+  $bruker = $q->fetch(PDO::FETCH_OBJ);
+
+  if (isset($bruker->passord)) {
+    if (password_verify($_REQUEST['passord'], $bruker->passord)) {
+
+      $_SESSION["id"] = $bruker->id;
+      $_SESSION["brukernavn"] = $bruker->brukernavn;
+      $_SESSION["passord"] = $bruker->passord;
+      $_SESSION["fnavn"] = $bruker->fnavn;
+      $_SESSION["enavn"] = $bruker->enavn;
+      $_SESSION["rolle"] = $bruker->rolle;
+      $_SESSION['login'] = 1;
+
+      header("Location:pages/hjem.php");
+      exit();
+    } else {
+      $messages[] = "Brukernavn og/eller passord er galt";
+    }
+  } else {
+    $messages[] = "Brukernavn og/eller passord er galt";
+  }
+}
 ?>
 
 
@@ -19,61 +68,27 @@ $conn = mysqli_connect('localhost', 'root', '', 'hybelprosjekt') or die("Kunne i
   <div class="login">
     <h1>H Y B B E L . N O</h1>
     <form method="post" action="" method="post">
-      <p><input type="text" name="brukernavn" value="" placeholder="Brukernavn eller e-post"></p>
-      <p><input type="password" name="passord" value="" placeholder="Passord"></p>
+      <p><input type="text" name="brukernavn" placeholder="Brukernavn eller e-post"></p>
+      <p><input type="password" name="passord" placeholder="Passord"></p>
       <p class="remember_me">
         <label>
           <input type="checkbox" name="remember_me" id="remember_me">
-          Husk meg på denne enheten
+          Ikke bruker? Registrer deg her!
         </label>
       </p>
       <p class="submit"><input type="submit" name="login" value="Logg inn"></p>
     </form>
   </div>
 
-  <div class="login-help">
-    <p>Glemt passord? <a href="#">Klikk her for å tilbakestille</a>.</p>
-  </div>
+  <?php
+  //$pass = 12345;
+  //echo password_hash($pass, PASSWORD_DEFAULT);
+  ?>
+
   <?php
   include "assets/inc/footer.php";
   ?>
-  <?php
-
-  $brukernavn = " ";
-  $passord = " ";
-
-  if (isset($_POST['login'])) {
-    $brukernavn = $_POST['brukernavn'];
-    $passord = $_POST['passord'];
-  }
-
-
-  $select = mysqli_query($conn, "SELECT id, brukernavn, passord, fnavn, enavn, rolle FROM brukere WHERE brukernavn = '$brukernavn' AND passord = '$passord'");
-  $row = mysqli_fetch_array($select);
-
-  if (is_array($row)) {
-
-    $_SESSION["id"] = $row['id'];
-    $_SESSION["brukernavn"] = $row['brukernavn'];
-    $_SESSION["passord"] = $row['passord'];
-    $_SESSION["fnavn"] = $row['fnavn'];
-    $_SESSION["enavn"] = $row['enavn'];
-    $_SESSION["rolle"] = $row['rolle'];
-    /*if ($row['b_admin'] = "admin") {
-      $_SESSION["admin"] = 1;
-    } else {
-      $_SESSION["admin"] = 0;
-    }*/
-
-    $_SESSION["login"] = 1;
-  } else echo "";
-
-  if (isset($_SESSION["brukernavn"])) {
-    header("Location:pages/hjem.php");
-  } else die;
-  ?>
-
-
+  
 </body>
 
 </html>
